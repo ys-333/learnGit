6b0e9b4fba61cc15998bdcaf8c7feba8f5165d1c
Yashwant Singh <yashwantsing333@gmail.com>
2021-06-01 15:28:37 +0530

    adding diif.cpp

---
+++ diif.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b;
+    cout<<"Enter the numbre to be subtracted "<<endl;
+    cin>>a>>b;
+    int res=a-b;
+    cout<<"The differnce of two number is as follow "<<res<<endl;
+    
+}
